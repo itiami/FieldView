5054af2405aed6d64f3f26532fbc221dbb1acd99
itiami <info@wali.co>
2025-10-26 18:57:01 +0100

    Added Prefix for Angular App

---
+++ infra/web/ng.ps1
@@ -0,0 +1,9 @@
+cd web-ngular
+ng g m features/realtime --routing
+ng g m features/users --routing
+ng g m features/reports --routing
+ng g m features/documents --routing
+ng g m core
+ng g m shared
+
+
